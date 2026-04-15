d8e1479151791654947dc076b3adb5baf665eed4
jayashreepandu1520-netizen <jayashreepandu1520@gmail.com>
2026-04-15 12:01:27 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,28 +1,28 @@
 import streamlit as st
-from greg import Greg
+from groq import Groq
 
-st.set_page_config("PrapgenAI Content Generator", layout="wide")
-st.title("PrapgenAI - Content Generator")
-st.image("hibiscus.png")
-client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+st.set_page_config("PragyanAI Content Generator",layout="wide")
+st.title("PragyanAI - Context Generator")
+st.image("Screenshot 2025-12-24 082406.png")
+client=Groq(api_key=st.secrets["GROQ_API_KEY"])
 product = st.text_input("Product")
 audience = st.text_input("Audience")
 if st.button("Generate Content"):
-    prompt = f"Write marketing content for {product} targeting {audience}."
-    response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
-        messages=[{"role": "user", "content": prompt}]
-    )
-    st.session_state.text = response.choices[0].message.content
-    text =response.choices[0].message.content
-    st.write(text)
-if "text" in st.session_state:
-    content = st.text_area("Generated Content", st.session_state.text, height=300)
-    st.download_button(
-            label="⬇️ Download as TXT",
-            data=content,
-            file_name="marketing_copy.txt",
-            mime="text/plain"
-        )
-    else:
-        st.info("Generate content first")
+  prompt = f"Write marketing content for {product} targeting {audience}."
+  response = client.chat.chat.completions.create(
+  model ="llama-3.3-70b-versatile",
+  messages=[{"role": "user", "content": prompt}]
+  )
+  st.session_stste.text = response.choices[0].message.content
+  text =response.choice[0].message.content
+  st.write(text)
+  if "text" in st.session_state:
+      content = st.text_area("Generated Content", st.ession_state.text, height=300)
+      st.download_button(
+        label="Doumload as TXT",
+        data=content,
+        file_name="marketing_copy.txt",
+        mime="text/plain"
+      )
+  else:
+       st.info("Generate content first")
